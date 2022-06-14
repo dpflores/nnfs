f8dde1dcf45a1070c6697a3c0cad277a0b978358
Del Piero Flores <delpiero22.flores@gmail.com>
2022-06-14 16:06:20 -0500

    RMSprop optimizer

--- mynnfs.py
+++ mynnfs.py
@@ -143,7 +143,7 @@ class Optimizer_SGD:
     # Call once before any parameter updates
     def pre_update_params(self):
         if self.decay:
-            self.current_learning_rate = self.learning_rate * (1 / (1 + self.decay * self.iterations))
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
 
     # Update parameters
     def update_params(self, layer):
@@ -183,6 +183,7 @@ class Optimizer_Adagrad:
 
     # Initialize optimizer - set settings,
     # learning rate of 1. is default for this optimizer
+    # Epsilon is another hyperparameter to avoid division by 0
     def __init__(self, learning_rate=1.0, decay=0., epsilon=1e-7):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
@@ -193,7 +194,7 @@ class Optimizer_Adagrad:
     # Call once before any parameter updates
     def pre_update_params(self):
         if self.decay:
-            self.current_learning_rate = self.learning_rate * (1 / (1 + self.decay * self.iterations))
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
 
     # Update parameters
     def update_params(self, layer):
@@ -218,3 +219,44 @@ class Optimizer_Adagrad:
     def post_update_params(self):
         self.iterations += 1
 
+
+class Optimizer_RMSprop:
+
+    # Initialize optimizer - set settings,
+    # learning rate of 1. by default could cause instant model instability, so 0.001 is good here
+    # rho is the cache memory decay rate
+    def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, rho = 0.9):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.rho = rho
+
+    # Call once before any parameter updates
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+
+    # Update parameters
+    def update_params(self, layer):
+
+        # If layer does not contain cache arrays.
+        # create them filled with zeros
+        if not hasattr(layer, 'weight_cache'): # hasattr checks if that attribute exist
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        # Update cache with squared current gradients
+        layer.weight_cache = self.rho * layer.weight_cache +  (1-self.rho)*layer.dweights**2
+        layer.bias_cache = self.rho * layer.bias_cache + (1-self.rho)*layer.dbiases**2
+
+        # Vanilla SGD parameter update + normalization 
+        # with squared root cache
+        layer.weights += -self.current_learning_rate * layer.dweights/(np.sqrt(layer.weight_cache + self.epsilon))
+        layer.biases += -self.current_learning_rate * layer.dbiases/(np.sqrt(layer.bias_cache + self.epsilon))
+    
+
+    # Call once after any parameter updates
+    def post_update_params(self):
+        self.iterations += 1
