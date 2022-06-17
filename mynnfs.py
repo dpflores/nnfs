7afa7c5260730e463e9707be555a85d95c2e81f3
dpflores <delpiero22.flores@gmail.com>
2022-06-17 02:13:24 -0500

    regularization

--- mynnfs.py
+++ mynnfs.py
@@ -4,9 +4,17 @@ import numpy as np
 
 # OOP
 class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons, weight_regularizer_l1=0, weight_regularizer_l2=0, 
+                bias_regularizer_l1=0, bias_regularizer_l2=0):
+        # Initialize weights and biases 
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        # Set regularization strength
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
+
     def forward(self, inputs):
         # Remember input values
         self.inputs = inputs
@@ -15,6 +23,30 @@ class Layer_Dense:
         # Gradients on parameters
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradients on regularization
+        # For L1, lambda1 * abs'(x) = {1 x>0; -1 x<0}
+        # For L2, lammda2 * sum(x^2)' = 2 * x
+        # L1 on weights  
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+
+        # L2 on weights
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.weights
+
+        # L1 on biases
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += 2 * self.bias_regularizer_l1 * self.weights
+        
+        # L2 on biases
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+
         # Gradients on values
         self.dinputs = np.dot(dvalues, self.weights.T)
 
@@ -60,6 +92,30 @@ class Loss:
         data_loss = np.mean(sample_losses) # Gets the mean of all the losses for each sample
         return data_loss
 
+    # Regularization loss calculation
+    def regularization_loss(self, layer):
+
+        # 0 by default
+        regularization_loss = 0
+
+        # L1 regularization
+        # calculate only when factor greater than 0
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+
+        # L2 regularization - weights
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * np.sum(np.abs(layer.weights*layer.weights))
+        
+        # L1 regularization - biases
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.bias))
+        
+        # L2 regularization - biases
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * np.sum(np.abs(layer.biases*layer.biases))
+
+        return regularization_loss
 
 class Loss_CategoricalCrossentropy(Loss): #Inherited from the Loss Class
     # Forward pass
