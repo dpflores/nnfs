3500d30fa0d657e479eaee7898d7bef46f114fd8
dpflores <delpiero22.flores@gmail.com>
2022-06-28 16:27:01 -0500

    real dataset downloaded

---
+++ p19_real_dataset_download.py
@@ -0,0 +1,23 @@
+# Lets use a real dataset introduced by nnfs.io
+
+import os
+import urllib
+import urllib.request
+from zipfile import ZipFile
+
+URL = 'https://nnfs.io/datasets/fashion_mnist_images.zip'
+FILE = 'fashion_mnist_images.zip'
+FOLDER = 'fashion_mnist_images'
+
+# Gets the dataset from the link if already not downloaded
+if not os.path.isfile(FILE):
+    print(f'Downloading {URL} and saving as {FILE} ...')
+    urllib.request.urlretrieve(URL, FILE)
+
+# Unzip the dataset in the folder required
+
+print('Unzipping images...')
+with ZipFile(FILE) as zip_images:
+    zip_images.extractall(FOLDER)
+
+print('Done!')
